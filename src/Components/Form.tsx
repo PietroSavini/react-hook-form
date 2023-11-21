07314eb19ad356767a02c9a6e8f9470b0150635c
Savini Pietro <savinipietro@gmail.com>
2023-11-21 10:05:01 +0100

    form/ validation & error messages

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,26 +1,42 @@
 import { TextField, MenuItem, Button } from '@mui/material'
 import { useForm } from 'react-hook-form'
-
+import { DevTool } from '@hookform/devtools';
 
 export const Form = () => {
-    const form = useForm();
-    const { register } = form;
-    
+    type data = {
+        username:string;
+        password:string;
+        type:number;
+    }
+
+    const form = useForm<data>();
+    const { register, control, handleSubmit, formState } = form;
+    const { errors } = formState;
+    const onSubmit = (data:data) =>{
+        console.log('form submitted',data);
+    }
 
 
   return (
-    <div className='form'>
-
-        <TextField {...register("username")} sx={{marginBottom:'10px'}} type='text' label='username' id='username' name='username' />
+    <form onSubmit={handleSubmit(onSubmit)} noValidate>
+        <div className="form-control">
+            <TextField {...register("username", {required: "username is required"})} sx={{marginBottom:'10px'}} type='text' label='username' id='username' name='username' />
+            <p className='error'>{errors.username?.message}</p>
+        </div>
         <br />
-        <TextField {...register("password")} sx={{marginBottom:'10px'}} type='password' label='password' id='password' name='password'  />
+        <div className="form-control">
+            <TextField {...register("password", {required: "password is required"})} sx={{marginBottom:'10px'}} type='password' label='password' id='password' name='password'  />
+            <p className='error'>{errors.password?.message}</p>
+        </div>
         <br />
-        <TextField {...register("type")} select sx={{width:210}} id='type' name='type' label='tipo'  >
+        <TextField {...register("type") } select sx={{width:210}} id='type' name='type' label='tipo'  >
             <MenuItem key={1} value={1}>uno</MenuItem>
+            <MenuItem key={2} value={2}>due</MenuItem>
+            <MenuItem key={3} value={3}>tre</MenuItem>
         </TextField>
         <br />
-        <Button sx={{marginTop:'20px', }} color='warning' variant='contained'>submit</Button>
-           
-    </div>
+        <Button type='submit' sx={{marginTop:'20px', }} color='warning' variant='contained'>submit</Button>
+        <DevTool control={ control }/>
+    </form>
   )
 }
