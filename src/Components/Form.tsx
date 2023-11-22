3f04979642f782c3845db31510fa24f22ffdc7ba
Savini Pietro <savinipietro@gmail.com>
2023-11-22 08:16:21 +0100

    add validation to form

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -4,24 +4,24 @@ import { DevTool } from '@hookform/devtools';
 import { useEffect } from 'react';
 
 export const Form = () => {
+
     type data = {
-        username:string;
-        password:string;
-        type:number;
+        username: string;
+        password: string;
     }
 
     const form = useForm<data>();
     const { register, control, handleSubmit, formState, reset } = form;
     const { errors, isSubmitSuccessful } = formState;
 
-    const onSubmit = (data:data) =>{
+    const onSubmit = (data: data) =>{
         console.log('form submitted',data);
-        
-    }
-    const onError = (errors: FieldErrors<data>) => {
-        console.log("errori: ",errors)
     }
 
+    const onError = (errors: FieldErrors<any>) => {
+        console.log("errori: ",errors)
+    }
+    //reset del form una volta che isSubmitSuccessful cambia il valore da false a true
     useEffect(()=>{
         reset()
     },[isSubmitSuccessful])
@@ -29,12 +29,23 @@ export const Form = () => {
   return (
     <form onSubmit={handleSubmit(onSubmit, onError)} noValidate>
         <Stack spacing={2}>
-        <TextField error={!!errors.username} helperText={errors.username?.message} {...register("username", {required: "username is required"})} sx={{marginBottom:'10px'}} type='text' label='username' id='username' name='username' />
-        <TextField error={!!errors.password} helperText={errors.password?.message} {...register("password", {required: "password is required"})} sx={{marginBottom:'10px'}} type='password' label='password' id='password' name='password'  />
-        
-        <Button type='submit' sx={{marginTop:'20px', }} color='warning' variant='contained'>submit</Button>
+
+            <TextField 
+                sx={{marginBottom:'10px'}} 
+                error={!!errors.username} 
+                helperText={errors.username?.message} 
+                {...register("username", {required: "username is required"})} 
+                type='text' 
+                label='username' 
+                id='username' 
+                name='username' 
+            />
+            
+            
+            <Button type='submit' sx={{marginTop:'20px', }} color='warning' variant='contained'>submit</Button>
         
         </Stack >
+
         <DevTool control={ control }/>
     </form>
   )
